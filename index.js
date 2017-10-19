451139ad444ce304e575b101dcb73aaa625a42a6
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-19 13:18:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const returnFirstTwoDrivers = function (drivers) {
 const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(-2)
 }
+
+const selectingDrivers = [
+  returnFirstTwoDrivers()
+  returnLastTwoDrivers()
+]
